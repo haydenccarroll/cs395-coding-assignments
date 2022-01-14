53b4fd0a6e96604096845b04b60b6e37b2157ab0
Hayden Carroll <haydencarroll77@gmail.com>
2022-01-14 12:02:27 -0800

    added some changes to w01

--- w01.c
+++ w01.c
@@ -1,30 +1,37 @@
+// name: w01.c
+// author: hayden carroll
+// date-last-modified: 1/14/22
+// description: implementation of shortest distance between two points in an array
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// main function that takes in command line arguments such as ./w01.exe 1 2 65 3..
 int main(int argc, char *argv[])
 {
-    if (argc <= 2)
-    {
-        printf("%s 4 9 6 2\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    int myArray[argc -1];
-    for (int i=1; i < argc; i++)
-    {
-        myArray[i-1] = atoi(argv[i]);
-    }
-    int lowestDistance = abs(myArray[1] - myArray[0]);
+   if (argc <= 2)
+   {
+      printf("%s 4 9 6 2\n", argv[0]);
+      return EXIT_FAILURE;
+   }
+   int myArray[argc -1];
+   for (int i=1; i < argc; i++)
+   {
+      myArray[i-1] = atoi(argv[i]);
+   }
+   int lowestDistance = abs(myArray[1] - myArray[0]);
 
-    for (int i=0; i < argc-1; i++)
-    {
-        for (int z=0; z < argc-1; z++)
-        {
-            if (i != z && abs(myArray[i] - myArray[z]) < lowestDistance)
-            {
-                lowestDistance = abs(myArray[i] - myArray[z]);
-            }
-        }
-    }
-    printf("%d\n", lowestDistance);
-    return EXIT_SUCCESS;
+   for (int i=0; i < argc-1; i++)
+   {
+      for (int z=0; z < argc-1; z++)
+      {
+         if (i != z && abs(myArray[i] - myArray[z]) < lowestDistance)
+         {
+            lowestDistance = abs(myArray[i] - myArray[z]);
+         }
+      }
+   }
+   printf("%d\n", lowestDistance);
+   return EXIT_SUCCESS;
 }
