3c41c6dba7b73d69b705589d52c28591a8e7593b
Hayden Carroll <haydencarroll77@gmail.com>
2022-01-15 16:11:35 -0800

    added w08

---
+++ w08.c
@@ -0,0 +1,39 @@
+/*********************************************
+ * Id: carr5440
+ *
+ * Compile: gcc -Wall
+ * Run: ./a.out input.txt
+ *
+ * Brute force string match
+ *********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// main function
+int main(int argc, char *argv[])
+{
+   char* pattern = argv[1];
+   char* string = argv[2];
+
+   int i;
+   printf("match found at: ");
+   for (i=0; string[i] != '\0'; i++)
+   {
+      int j;
+      int matchFound = 1;
+      for (j=0; pattern[j] != '\0'; j++)
+      {
+         if (string[i+j] != pattern[j])
+         {
+            matchFound = 0;
+         }
+      }
+      if (matchFound == 1)
+      {
+         printf("%d ", i);
+      }
+   }
+   printf("\n");
+   return EXIT_SUCCESS;
+}
