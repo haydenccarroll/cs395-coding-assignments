d7e86536f8ea62f20b7d946d9f2001d04299f2eb
Hayden Carroll <haydencarroll77@gmail.com>
2022-01-16 15:39:08 -0800

    added w11

---
+++ w11.c
@@ -0,0 +1,61 @@
+/*********************************************
+ * Id: carr5440
+ *
+ * Compile: gcc -Wall
+ * Run: ./a.out input.txt
+ *
+ * Insertion Sort
+ *********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintArray(int myArray[], int arraySize);
+void InsertionSort(int myArray[], int arraySize);
+
+// main function
+int main(int argc, char *argv[])
+{
+   int arraySize = argc-1;
+   int myArray[arraySize];
+
+   int i;
+   for (i=0; i < arraySize; i++)
+   {
+      myArray[i] = atoi(argv[i+1]);
+   }
+
+   InsertionSort(myArray, arraySize);
+
+   return EXIT_SUCCESS;
+}
+
+// prints the array
+void PrintArray(int myArray[], int arraySize)
+{
+   int i;
+   for (i = 0; i < arraySize; i++)
+   {
+      printf("%d ", myArray[i]);
+   }
+   printf("\n");
+}
+
+//implementation of the insertion sort sorting algorithm
+void InsertionSort(int myArray[], int arraySize)
+{
+   PrintArray(myArray, arraySize);
+   int i;
+   for (i=1; i < arraySize; i++)
+   {
+      int v =  myArray[i]; // current value of array
+      int j = i -1;
+      while (j >= 0 && myArray[j] > v)
+      {
+         myArray[j+1] = myArray[j];
+         j--;
+      }
+      myArray[j+1] = v;
+   }
+   PrintArray(myArray, arraySize);
+}
