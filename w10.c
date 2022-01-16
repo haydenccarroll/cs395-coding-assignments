adff84f1c4c249453fde91eb07b78f03d738a155
Hayden Carroll <haydencarroll77@gmail.com>
2022-01-16 14:59:55 -0800

    spaces

--- w10.c
+++ w10.c
@@ -34,47 +34,47 @@ int main(int argc, char *argv[])
 // and right side greater
 int HoarePartition(int arr[], int low, int high) 
 { 
-    int pivot = arr[low];
+   int pivot = arr[low];
 
-    int i = low-1, j = high+1;
-    while(1)
-    {
-        do // find index where left ptr is greater than or equal to pivot
-        {
-            i++;
-        } while(arr[i] < pivot);
-        do // find index where right ptr is less than or equal to pivot
-        {
-            j--;
-        } while(arr[j] > pivot);
+   int i = low-1, j = high+1;
+   while(1)
+   {
+      do // find index where left ptr is greater than or equal to pivot
+      {
+         i++;
+      } while(arr[i] < pivot);
+      do // find index where right ptr is less than or equal to pivot
+      {
+         j--;
+      } while(arr[j] > pivot);
 
-        if(i >= j)
-        {
+      if(i >= j)
+      {
             // if ptrs touch, that means you covered the whole list, 
             // and everything to left is less than and everything to right is greater than
-            return j;
-        }
-        int tempI = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tempI;
-    }
-} 
+         return j;
+      }
+      int tempI = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tempI;
+   }
+}
 
 // implements quicksort
 void QuickSort(int arr[], int low, int high, int arraySize) 
 { 
-    if (low < high) 
-    { 
+   if (low < high) 
+   { 
         /* pi is partitioning index, arr[p] is now 
         at right place */
-        int split = HoarePartition(arr, low, high); 
+      int split = HoarePartition(arr, low, high); 
 
         // Separately sort elements before 
         // partition and after partition 
-        QuickSort(arr, low, split, arraySize);
-        QuickSort(arr, split + 1, high, arraySize);
-        PrintArray(arr, low, high, arraySize);
-    } 
+      QuickSort(arr, low, split, arraySize);
+      QuickSort(arr, split + 1, high, arraySize);
+      PrintArray(arr, low, high, arraySize);
+   } 
 } 
 // prints the array
 void PrintArray(int myArray[], int low, int high, int arraySize)
@@ -82,17 +82,17 @@ void PrintArray(int myArray[], int low, int high, int arraySize)
    int i;
    for (i = 0; i < arraySize; i++)
    {
-       if (i == low)
-       {
-           printf("[%d ", myArray[i]);
-       }
-       else if (i == high)
-       {
-           printf("%d] ", myArray[i]);
-       }
-       else {
-           printf("%d ", myArray[i]);
-       }
+      if (i == low)
+      {
+         printf("[%d ", myArray[i]);
+      }
+      else if (i == high)
+      {
+         printf("%d] ", myArray[i]);
+      }
+      else {
+         printf("%d ", myArray[i]);
+     }
    }
    printf("\n");
 }
